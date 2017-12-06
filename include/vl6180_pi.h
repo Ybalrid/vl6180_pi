202a4d1444b374def4171d37f4885a3ee0945805
Arthur Brainville (Ybalrid) <ybalrid@ybalrid.info>
2017-12-06 13:24:14 +0000

    deactivate hack

--- include/vl6180_pi.h
+++ include/vl6180_pi.h
@@ -28,11 +28,13 @@ int get_distance(vl6180 handle);
 /// \param handle The handle to the sensor given by vl6180_initialise
 /// \param scaling Index of the scaling mode to use
 void set_scaling(vl6180 handle, int scaling);
-  
+
+/*
 //hack:add access to lower_level functions
 int read_byte(vl6180 handle, int reg);
 void write_byte(vl6180 handle, int reg, char data);
 void write_two_bytes(vl6180 handle, int reg, int data);
+*/
 
 #ifdef __cplusplus
 }
